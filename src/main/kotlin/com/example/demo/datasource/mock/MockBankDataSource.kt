d95893fbcd3b5bbb28645d3bd80b857bdf961be7
sainu <sainu@users.noreply.github.com>
2022-12-31 17:15:31 +0900

    Create BankDataSource

---
+++ src/main/kotlin/com/example/demo/datasource/mock/MockBankDataSource.kt
@@ -0,0 +1,16 @@
+package com.example.demo.datasource.mock
+
+import com.example.demo.datasource.BankDataSource
+import com.example.demo.model.Bank
+import org.springframework.stereotype.Repository
+
+@Repository
+class MockBankDataSource : BankDataSource {
+    val banks =  listOf(
+        Bank(accountNumber = "1234", trust = 3.14, transactionFree = 17),
+        Bank(accountNumber = "1010", trust = 17.0, transactionFree = 0),
+        Bank(accountNumber = "5678", trust = 0.0, transactionFree = 100),
+    )
+
+    override fun retrieveBanks(): Collection<Bank> = banks
+}
